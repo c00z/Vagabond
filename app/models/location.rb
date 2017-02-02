69b94ecf1ce455f6679909f412dac2716c9e34ce
ricarellano <rarellanob@gmail.com>
2017-02-02 15:08:10 -0800

    view city pages for London and Gibraltad

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,9 @@
 class Location < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :users, through: :posts
+
+  # Add to the location model...note: we probably want to change the default sizes
+
+  has_attached_file :avatar, styles: { medium: "1000x400>", thumb: "250x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,8 +18,20 @@ p "Hello from seeds.rb"
 
 Post.delete_all
 Location.delete_all
-Location.create({name: "San Francisco"})
 
+
+first = Location.create({name: "San Francisco"})
+second = Location.create({name: "London"})
+third = Location.create({name: "Gibraltar"})
+fourth = Location.create({name: "Tokyo"})
+first.avatar = File.open("#{Rails.root}/app/assets/images/SF.gif")
+second.avatar = File.open("#{Rails.root}/app/assets/images/London.gif")
+third.avatar = File.open("#{Rails.root}/app/assets/images/Gibraltar.gif")
+fourth.avatar = File.open("#{Rails.root}/app/assets/images/Tokyo.gif")
+first.save
+second.save
+third.save
+fourth.save
 
 posts_data = [
 {:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.first},
@@ -27,7 +39,26 @@ posts_data = [
 {:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.first},
 {:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.first},
 {:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.first},
-{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.first}
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.second},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.second},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.second},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.second},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.second},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.second},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.third},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.third},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.third},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.third},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.third},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.third},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.third},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.fourth},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.fourth},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.fourth},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.fourth},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.fourth},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.fourth},
+{:title=>FFaker::Book.title, :content=>FFaker::CheesyLingo.paragraph, :user=> User.first, :location=> Location.fourth}
 ]
 
 post = Post.create(posts_data)
