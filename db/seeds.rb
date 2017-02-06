c578a6d6231fd02b63f72f385a0995e17037e782
c00z <zcusimano@gmail.com>
2017-02-05 18:41:10 -0800

    removed dead code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,5 @@
 p "Hello from seeds.rb"
 
-# Speaker.delete_all
-#
-# speakers_data = [
-#     {:first=>"Cory", :last=>"Fauver", :email=>"cory@example.com"},
-#     {:first=>"Brianna", :last=>"Veenstra", :email=>"brianna@example.com"},
-#     {:first=>"Matt", :last=>"Laguardia", :email=>"matt@example.com"},
-#     {:first=>"Ilias", :last=>"Tsangaris", :email=>"ilias@example.com"},
-#     {:first=>FFaker::Name.first_name, :last=>FFaker::Name.last_name, :email=>FFaker::Internet.email},
-#     {:first=>FFaker::Name.first_name, :last=>FFaker::Name.last_name, :email=>FFaker::Internet.email},
-#     {:first=>FFaker::Name.first_name, :last=>FFaker::Name.last_name, :email=>FFaker::Internet.email},
-#     {:first=>FFaker::Name.first_name, :last=>FFaker::Name.last_name, :email=>FFaker::Internet.email},
-#     {:first=>FFaker::Name.first_name, :last=>FFaker::Name.last_name, :email=>FFaker::Internet.email}
-# ]
-#
-# Speaker.create(speakers_data)
-
 Post.delete_all
 Location.delete_all
 
@@ -65,13 +49,3 @@ post = Post.create(posts_data)
 post.each do |post|
   p post.latitude
 end
-
-#
-# Post.delete_all
-#  posts = []
-# 10.times do |i|
-#   posts<<(
-#     {:title=>FFaker::Book.title,
-#      :content=>FFaker::BaconIpsum.paragraphs})
-# end
-# Post.create([posts])
