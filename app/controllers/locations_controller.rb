ffb3cb230f7f8d1e04ccbe0dd05e46a3d4526d09
johnson-rl <johnson.rl@gmail.com>
2017-02-05 18:27:43 -0800

    activities now ranking

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,6 +6,19 @@ class LocationsController < ApplicationController
 
   def show
     @location = Location.find_by_id(params[:id])
+    all_activities = @location.activity_count
+    @activity_cards = top_activities all_activities
+    p @activity_cards
+  end
+
+  private
+
+  def top_activities acts
+    top_five = []
+    acts.values.sort.reverse.each do |i|
+      top_five.push(acts.select{|k,v| v == i}).uniq!
+    end
+    top_five[0..4]
   end
 
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -6,4 +6,27 @@ class Location < ApplicationRecord
 
   has_attached_file :avatar, styles: { medium: "1000x400>", thumb: "250x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  def activity_count
+    activity_counter = {}
+    self.posts.each do |p|
+      p.activities.each do |a|
+        if activity_counter.has_key?(a.name)
+          activity_counter[a.name] += 1
+        else
+          activity_counter[a.name] = 1
+        end
+      end
+    end
+    activity_counter
+  end
+
+  # def top_activities acts
+  #   top_five = []
+  #   acts.values.sort.reverse.each do |i|
+  #     top_five.push(acts.select{|k,v| k == i})
+  #   end
+  #   top_five[0..4]
+  #   binding.pry
+  # end
 end
