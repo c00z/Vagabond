fdababc675480e78c8449305d6e767864a5168a3
johnson-rl <johnson.rl@gmail.com>
2017-02-06 15:56:13 -0800

    fixed edit user and duplicate username/email bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  
+
   validates :user_name, presence: true, uniqueness: true, length: {minimum: 4}
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: {minimum: 5}
 
 
   has_many :posts, dependent: :destroy
