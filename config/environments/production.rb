0bb69752ed2f64853daad31db8f6b2c5b04aaa96
johnson-rl <johnson.rl@gmail.com>
2017-03-23 14:50:52 -0700

    fixed typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
-    }
+    },
     Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-1.amazonaws.com'
   }
 
