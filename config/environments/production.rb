275910c315ca7660480a5875df2d60174ec93885
johnson-rl <johnson.rl@gmail.com>
2017-03-23 14:53:24 -0700

    different config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
     },
-    Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-1.amazonaws.com'
+    s3_host_name: 's3-us-west-1.amazonaws.com'
   }
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
